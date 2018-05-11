c2937c708ec5f9f8c93bcae325e6478b0560f105
Aryan <aryanebrahimpour@gmail.com>
2018-05-11 21:52:51 +0430

    RecordData Model

--- GuilanGame/Models/RecordModels.cs
+++ GuilanGame/Models/RecordModels.cs
@@ -7,16 +7,11 @@ using System.Threading.Tasks;
 namespace GuilanGame.Models
 {
 
-    public enum StudentFiled
-    {
-        Computer, Electrics, WaterEngineering, CivilEngineering, Mechanics, Medical, Textile, Humanitism, BaseScience, Other
-    }
-
     public class RecordItem
     {
         public int    Rank { get; set; }
         public string Name { get; set; }
         public int    Score { get; set; }
-        public StudentFiled StudentFiled { get; set;}
+        public string StudentFiled { get; set;}
     }
 }
